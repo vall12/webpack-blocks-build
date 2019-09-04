b83d94671b594a86b830e2e5279396d146555463
vall12 <vall12@yandex.ru>
2019-09-04 15:37:05 +0300

    added terserPlugin options

--- src/Lib/custom_webpack_blocks/jsTerserMinify.js
+++ src/Lib/custom_webpack_blocks/jsTerserMinify.js
@@ -6,11 +6,13 @@ const TerserPlugin = require('terser-webpack-plugin');
  * @returns {function(*, *): function(*): ({optimization}|*)}
  */
 function jsTerserMinify(terserOptions = {}) {
+    terserOptions = Object.assign({cache:true, parallel:4}, terserOptions);
+
     return function (context, utils) {
         return function (prevConfig) {
             prevConfig.optimization = prevConfig.optimization || {};
             prevConfig.optimization.minimizer = prevConfig.optimization.minimizer || [];
-            prevConfig.optimization.minimizer.push(new TerserPlugin());
+            prevConfig.optimization.minimizer.push(new TerserPlugin(terserOptions));
             return prevConfig;
         }
     }
